3c5711093a0bf88f08fd49a9e911c81dc2cc5d44
Grant Corley <grantcorley@gmail.com>
2021-07-24 21:22:56 -0400

    added animation to main headline

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 //script.js
 //IIFE
+
+// import smoothscroll from 'smoothscroll-polyfill';
+// // kick off the polyfill!
+// smoothscroll.polyfill();
+
 (function () { initialize();})();
 //------------------
 
@@ -149,6 +154,30 @@ function initialize(){
 		header.classList.remove("collapsed");
 		nav.classList.remove('menu_header_collapsed');
 	}
+
+
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+		  // Prevent default anchor click behavior
+		  event.preventDefault();
+	
+		  // Store hash
+		  var hash = this.hash;
+	
+		  // Using jQuery's animate() method to add smooth page scroll
+		  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		  $('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 800, function(){
+	
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		  });
+		} // End if
+	  });
 
 
 
